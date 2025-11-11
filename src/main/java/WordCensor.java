8a4eceea17809bd16b6463a677e2ded53b2a664d
BraydenV42 <brayriley44@gmail.com>
2025-11-11 18:34:05 -0500

    Update WordCensor.java

--- src/main/java/WordCensor.java
+++ src/main/java/WordCensor.java
@@ -2,9 +2,35 @@ import java.util.Scanner;
 
 public class WordCensor {
     public static void main(String[] args) {
-        // TODO: Read a full sentence from the user using Scanner.nextLine()
-        // TODO: Replace each banned word with exactly ***: dang, nuts, oops, yikes
-        // TODO: Print the result prefixed with: Censored:
-        // NOTE: Tests require exactly three asterisks (no ****) for every replacement
+        // Read a full sentence from the user
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        scanner.close();
+
+        String censored = "";
+        int i = 0;
+
+        while (i < sentence.length()) {
+            // Check for each banned word starting at position i
+            if (i + 4 <= sentence.length() && sentence.substring(i, i + 4).equals("dang")) {
+                censored += "***";
+                i += 4;
+            } else if (i + 4 <= sentence.length() && sentence.substring(i, i + 4).equals("nuts")) {
+                censored += "***";
+                i += 4;
+            } else if (i + 4 <= sentence.length() && sentence.substring(i, i + 4).equals("oops")) {
+                censored += "***";
+                i += 4;
+            } else if (i + 5 <= sentence.length() && sentence.substring(i, i + 5).equals("yikes")) {
+                censored += "***";
+                i += 5;
+            } else {
+                // Copy current character as-is
+                censored += sentence.charAt(i);
+                i++;
+            }
+        }
+
+        System.out.println("Censored: " + censored);
     }
 }
